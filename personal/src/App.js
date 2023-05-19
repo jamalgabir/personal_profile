38c8b8f5ddb023b894b87342033cb96dae54bcc9
jamalgabir <jamalgamy@gmail.com>
2023-05-19 16:04:06 +0100

    m

---
+++ personal/src/App.js
@@ -0,0 +1,10 @@
+import Header from "./component/header/header";
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  );
+}
+
+export default App;
---
+++ personal/src/component/header/header.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+import "./header.css"
+const header = () => {
+  return (
+    <div className='header-container'>
+      <div className="header1">
+        <h2>Jamal.Dev</h2>
+      </div>
+      <div className="header2">
+        <h3>Mern developer</h3>
+      </div>
+      <div className="header3">
+      
+      <ul className="sidebar-menu">
+        <li><a href="/home">Home</a></li>
+        <li><a href="/about">About Us</a></li>
+        <li><a href="/contact">Contact</a></li>
+      </ul>
+    
+      </div>
+    </div>
+  )
+}
+
+export default header
