967cadfc7246d29729b8503a7569d94935786ff0
jamalgabir <jamalgamy@gmail.com>
2023-05-20 12:23:36 +0100

    more batter

--- personal/src/App.js
+++ personal/src/App.js
@@ -1,9 +1,11 @@
 import Header from "./component/header/header";
-import Main from "./component/main/main"
+import Main from "./component/main/main";
+//const img = "https://images.pexels.com/photos/270366/pexels-photo-270366.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1";
+
 function App() {
   return (
     <div className="App">
-      {/* <Header/> */}
+      <Header/>
       <Main/>
     </div>
   );
